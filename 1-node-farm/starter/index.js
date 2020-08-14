3511fceb2792b77f6862d9999db1407f32016469
pasqualespica <pasqualespica@gmail.com>
2020-08-14 18:30:25 +0200

    add http module

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,21 +1,41 @@
 const fs = require("fs");
+const http = require("http")
+
+// /////////////////////////////////////////7
+// FILES
 
 // Blocking synchronous way
+
 // const textIn = fs.readFileSync('./txt/input.txt','utf-8');
 // console.log(textIn);
 // const textOut = `This is what we know about the avocado \n${textIn}\n ${Date.now()}`
 // fs.writeFileSync('./txt/output.txt', textOut);
 
 // NON -Blocking synchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-  // console.log(data)
-  fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-        fs.writeFile('./txt/final.txt', `${data2}\n${data3}` , 'utf-8', err => {
-            console.log(`Yeaahhh 😂`)
-        })
-    });
-  });
-});
 
-console.log("DONE!!");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   // console.log(data)
+//   fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+//     fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+//         fs.writeFile('./txt/final.txt', `${data2}\n${data3}` , 'utf-8', err => {
+//             console.log(`Yeaahhh 😂`)
+//         })
+//     });
+//   });
+// });
+// console.log("DONE!!");
+
+// /////////////////////////////////////////7
+// SERVER
+const server = http.createServer((req, res)=> {
+    console.log(req)
+    res.end("Hello from the server!!!")
+
+})
+
+const PORT=8000
+const HOST='127.0.0.1'
+server.listen(PORT, HOST, () => {
+
+    console.log(`Listening on ${HOST}:${PORT} `)
+})
