5118517b254adb5c8bcd26c0d1720c38c07d509c
pasqualespica <pasqualespica@gmail.com>
2020-08-17 17:19:37 +0200

    Consuming Promises with Async/Await

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -24,22 +24,44 @@ const writeFilePro = (file, data) => {
   });
 };
 
-readFilePro(`${__dirname}/dog.txt`)
-  .then((data) => {
+// Consuming Promises with Async/Await
+// ritorna automaticamente una Promise
+const getDogPic = async () => {
+  try {
+    const data = await readFilePro(`${__dirname}/dog.txt`);
     console.log(`Razza : ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-  .then((res) => {
-    console.log(`RES ${res.body.message}`);
-    return writeFilePro('dog-img.txt', res.body.message);
-    // fs.writeFile('dog-img.txt', res.body.message, (err) => {
-    //   if (err) return console.log(err.message);
-    //   console.log('Ramdom dog image save to file');
-    // });
-  })
-  .then((bahhh) => {
-    console.log('Dajeeeeeeeee Salvato !!!', bahhh);
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+
+    const res = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    console.log(res.body.message);
+
+    await writeFilePro('dog-img.txt', res.body.message);
+    console.log('SALVATO Dajee 🤩 ');
+  } catch (error) {
+    console.log(`OOOOOO errore ${error}`);
+  }
+};
+
+// Chiamata ...
+getDogPic();
+
+// readFilePro(`${__dirname}/dog.txt`)
+//   .then((data) => {
+//     console.log(`Razza : ${data}`);
+//     return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+//   })
+//   .then((res) => {
+//     console.log(`RES ${res.body.message}`);
+//     return writeFilePro('dog-img.txt', res.body.message);
+//     // fs.writeFile('dog-img.txt', res.body.message, (err) => {
+//     //   if (err) return console.log(err.message);
+//     //   console.log('Ramdom dog image save to file');
+//     // });
+//   })
+//   .then((bahhh) => {
+//     console.log('Dajeeeeeeeee Salvato !!!', bahhh);
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
