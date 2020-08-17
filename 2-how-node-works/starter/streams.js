75f6c61eeb517263af1c50cf08cc577cd524be1f
pasqualespica <pasqualespica@gmail.com>
2020-08-17 14:02:56 +0200

    add Stream  PIPE ...

---
+++ 2-how-node-works/starter/streams.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const server = require("http").createServer();
+
+server.on("request", (req, res) => {
+  // ----------------- solution 1
+  //   fs.readFile("./test-file.txt", (err, data) => {
+  //     if (err) console.log(err);
+  //     res.end(data);
+  //   });
+  // ----------------- solution 2 : Strems !!!
+  //   const readable = fs.createReadStream("./test-file.txt");
+  //   readable.on("data", (chunk) => {
+  //     res.write(chunk);
+  //   });
+  //   readable.on("end", () => {
+  //     res.end();
+  //   });
+  //   readable.on("error", (err) => {
+  //     console.log(err);
+  //     res.statusCode = 500;
+  //     res.end("File not found");
+  //   });
+  // ----------------- solution 3 : PIPE operator to resolve backpressoure problem
+  const readable = fs.createReadStream("./test-file.txt");
+  readable.pipe(res); // readableSource.pipe(writeableDestination)
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("waiting ...");
+});
