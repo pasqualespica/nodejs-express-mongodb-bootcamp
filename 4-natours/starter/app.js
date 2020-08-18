c9cfff3164491e9701961b1e38388607cbd5c942
pasqualespica <pasqualespica@gmail.com>
2020-08-18 15:05:45 +0200

    ENV

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -10,7 +10,10 @@ const app = express();
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // middleware a function that modify INCOMING data
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-app.use(morgan('dev'));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
+// console.log(process.env);
 
 // >>>>>>>>>>>x>>>> server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT} ...`);
 });
